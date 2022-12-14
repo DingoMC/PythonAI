aa305f96e207249a9f5ef58c77be74444048b826
DingoMC <martin701-2001@wp.pl>
2022-12-14 14:56:37 +0100

    lab8

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -1,8 +1,8 @@
 # %%
 # 8.1
-from keras.layers import Dense, Input, Reshape, BatchNormalization
-from keras.layers.merging import average
+from keras.layers import Dense, Input, Reshape, BatchNormalization, Average, Conv2D, Add, Activation
 from keras.models import Model
+import tensorflow as tf
 import numpy as np
 import pandas as pd
 from keras.datasets import mnist
@@ -43,7 +43,7 @@ def add_inseption_module(input_tensor):
         for layer in path:
             output_tensor = layer(output_tensor)
         for_concat.append(output_tensor)
-    return average(for_concat)
+    return for_concat
 data = mnist.load_data()
 X_train, y_train = data[0][0], data[0][1]
 X_test, y_test = data[1][0], data[1][1]
@@ -57,6 +57,7 @@ output_tensor = input_tensor = Input(input_shape)
 output_tensor = Reshape((784,))(output_tensor)
 output_tensor = BatchNormalization()(output_tensor)
 output_tensor = add_inseption_module(output_tensor)
+output_tensor = Average()(output_tensor)
 ANN = Model(inputs = input_tensor,
     outputs = output_tensor)
 ANN.compile(loss = 'categorical_crossentropy',
@@ -64,3 +65,26 @@ ANN.compile(loss = 'categorical_crossentropy',
     optimizer = 'adam')
 plot_model(ANN, show_shapes=True)
 # %%
+# 8.2
+def res_net (input_tensor):
+    skip_tensor = input_tensor
+    output_tensor = input_tensor
+    output_tensor = Conv2D(filters = 32, kernel_size=(3, 3), padding='same')(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
+    output_tensor = Add()
+    output_tensor = Activation('relu')(output_tensor)
+    return output_tensor
+output_tensor = input_tensor = Input(X_train.shape[1:])
+output_tensor = res_net(output_tensor)
+ANN = Model(inputs = input_tensor,
+    outputs = output_tensor)
+ANN.compile(loss = 'categorical_crossentropy',
+    metrics = 'accuracy',
+    optimizer = 'adam')
+plot_model(ANN, show_shapes=True)
+
+# %%
+# 8.4
+def Mish(x):
+    return x * tf.math.tanh(tf.math.log(1 + tf.math.exp(x)))
+    
